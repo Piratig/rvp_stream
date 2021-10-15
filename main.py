095f36f29bc00ae614f7504ffd1b23cb5528d951
Piratig <his1994@mail.ru>
2021-10-15 22:28:48 +0300

    Query fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 import dash
 import pymysql.cursors
 from dash import Input, Output, callback
@@ -12,24 +13,25 @@ conn = pymysql.connect(host='127.0.0.1',
                        passwd='password',
                        db='trains',
                        cursorclass=pymysql.cursors.DictCursor)
-cursor = conn.cursor()
+
 
 # Filtering DataFrame by line name
 def df_update(line_name, df):
     df = df.loc[df['line'] == "{0}".format(line_name)]
-    df = df.drop_duplicates(['headnum'], keep='last') # When the request changes, change to the keep='first'
+    df = df.drop_duplicates(['route'], keep='first') # When the request changes, change to the keep='first'
     df = df.sort_values('route', kind='mergesort')
     return df
 
 # Loading data from DB into DataFrame
 def db_query():
-    df_rvp_query = pd.read_sql("SELECT line, route, headnum, tcs, datearrival FROM train ORDER BY id DESC LIMIT 1", conn)
+    cursor = conn.cursor()
+    df_rvp_query = pd.read_sql("SELECT line, route, headnum, tcs, datearrival FROM train ORDER BY id DESC LIMIT 1000", conn)
     pd.set_option('display.expand_frame_repr', False)
     df_rvp_query['headnum'] = df_rvp_query['headnum'].astype(int)
     df_rvp_query = df_rvp_query.loc[df_rvp_query['headnum'] >= 0]
     df_rvp_query['datearrival'] = df_rvp_query['datearrival'].str[:19]
     df_rvp_query = df_rvp_query.drop(df_rvp_query[df_rvp_query.tcs == '[]'].index)
-    print(pd.read_sql("SELECT line, route, headnum, tcs, datearrival FROM train ORDER BY id DESC LIMIT 1", conn))
+    conn.commit()
     return df_rvp_query
 
 # Initialization
@@ -62,7 +64,6 @@ app.layout = html.Div(children=[
 @callback(Output('tbl', 'data'), [Input('time', 'n_intervals'), Input('dpd', 'value')])
 def update_graphs(n_intervals, value):
     df_rvp_stream = db_query()
-    print(df_rvp_stream)
     df_rvp_stream = df_update(value, df_rvp_stream)
     new_df = df_rvp_stream.to_dict('records')
     print('Table updated')
