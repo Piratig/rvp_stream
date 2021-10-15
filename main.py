c2dede8e46fa74f084396e4ad29b4a1cbc8f7c28
Piratig <his1994@mail.ru>
2021-10-15 23:08:03 +0300

    PEP_8 fixes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import dash
 import pymysql.cursors
 from dash import Input, Output, callback
@@ -18,14 +17,17 @@ conn = pymysql.connect(host='127.0.0.1',
 # Filtering DataFrame by line name
 def df_update(line_name, df):
     df = df.loc[df['line'] == "{0}".format(line_name)]
-    df = df.drop_duplicates(['route'], keep='first') # When the request changes, change to the keep='first'
+    df['route'] = df['route'].astype(int)
+    df = df.drop_duplicates(['route'], keep='first')
     df = df.sort_values('route', kind='mergesort')
     return df
+
 
 # Loading data from DB into DataFrame
 def db_query():
     cursor = conn.cursor()
-    df_rvp_query = pd.read_sql("SELECT line, route, headnum, tcs, datearrival FROM train ORDER BY id DESC LIMIT 1000", conn)
+    df_rvp_query = pd.read_sql("SELECT line, route, headnum, tcs, datearrival FROM train ORDER BY id DESC LIMIT 1000",
+                               conn)
     pd.set_option('display.expand_frame_repr', False)
     df_rvp_query['headnum'] = df_rvp_query['headnum'].astype(int)
     df_rvp_query = df_rvp_query.loc[df_rvp_query['headnum'] >= 0]
@@ -33,6 +35,7 @@ def db_query():
     df_rvp_query = df_rvp_query.drop(df_rvp_query[df_rvp_query.tcs == '[]'].index)
     conn.commit()
     return df_rvp_query
+
 
 # Initialization
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -44,7 +47,7 @@ df_rvp_stream = df_update('', df_rvp_stream)
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(children=[
     html.H4(children='РВП данные', style={'textAlign': 'center', 'color': '#111111'}),
-    dcc.Dropdown(id = 'dpd',
+    dcc.Dropdown(id='dpd',
                  options=[
                      {'label': 'SLL', 'value': 'sllLine'},
                      {'label': 'NKL', 'value': 'nklLine'}
@@ -54,12 +57,16 @@ app.layout = html.Div(children=[
     dt.DataTable(id='tbl', style_data={'whiteSpace': 'normal', 'height': 'auto', },
                  data=df_rvp_stream.to_dict('records'),
                  columns=[{"name": i, "id": i} for i in df_rvp_stream.columns],
-                 style_cell = {'textAlign': 'left'},
+                 style_cell={'textAlign': 'left'},
+                 style_cell_conditional=[{'if': {'column_id': 'line'}, 'width': '5%'},
+                                         {'if': {'column_id': 'route'}, 'width': '5%'},
+                                         {'if': {'column_id': 'headnum'}, 'width': '5%'}]
                  ),
     dcc.Interval(id='time',
                  interval=5 * 1000,
                  n_intervals=0),
 ])
+
 
 @callback(Output('tbl', 'data'), [Input('time', 'n_intervals'), Input('dpd', 'value')])
 def update_graphs(n_intervals, value):
